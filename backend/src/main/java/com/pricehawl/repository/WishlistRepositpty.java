77c6941fb682d0a6862431fc2e5b5d02cefcab92
Nguyenthuyhangm <23020662@vnu.edu.vn>
2026-04-11 14:43:07 +0700

    add repository WL

---
+++ backend/src/main/java/com/pricehawl/repository/WishlistRepositpty.java
@@ -0,0 +1,17 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, UUID> {
+    // Lấy danh sách wishlist của 1 user cụ thể
+    List<Wishlist> findByUserId(UUID userId);
+    
+    // Tìm để xóa đúng sản phẩm của user đó
+    void deleteByUserIdAndProductId(UUID userId, UUID productId);
+    
+    // Kiểm tra xem user đã thích sản phẩm này chưa
+    boolean existsByUserIdAndProductId(UUID userId, UUID productId);
+}
