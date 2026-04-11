46b70fa5878fabc540253bfece76237a29d6cd6f
Nguyenthuyhangm <23020662@vnu.edu.vn>
2026-04-11 14:42:02 +0700

    add entity wishlist

---
+++ backend/src/main/java/com/pricehawl/entity/Wishlist.java
@@ -0,0 +1,26 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wishlist")
+@Data
+public class Wishlist {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "product_id", nullable = false)
+    private UUID productId;
+
+    @Column(name = "added_at")
+    private LocalDateTime addedAt = LocalDateTime.now();
+}
